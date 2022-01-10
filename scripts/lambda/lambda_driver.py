436198d59ce95337a3727737ef75438baad5b8f3
RV <46471006+rv-rescala@users.noreply.github.com>
2022-01-11 00:43:05 +0900

    change path st

--- scripts/lambda/lambda_driver.py
+++ scripts/lambda/lambda_driver.py
@@ -16,17 +16,53 @@ def lambda_handler(event, context):
         key = event["Records"][0]["s3"]["object"]["key"]
         trigger_name = key.split(".")[0].split("/")[-1]
         print(trigger_name)
+        print("event_source from s3 end")
         # init func
         actor_driver_starter(bucket=bucket, prefix=prefix, actor_conf_file=actor_conf_file, trigger_name=trigger_name)
-    # aws:s3
-    
-    try:
+    elif event_source == "aws:sqs":
         driver_trigger_message_str = event["Records"][0]["body"]
-    except:
+        actor_driver(bucket="captool", prefix="conf", actor_conf_file="actor_conf.json", driver_trigger_message_str=driver_trigger_message_str)
+        print("event_source from sqs end")
+    else:
         driver_trigger_message_str = None
+        actor_driver(bucket="captool", prefix="conf", actor_conf_file="actor_conf.json", driver_trigger_message_str=driver_trigger_message_str)
+        print("event_source from manural end")
+        
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
+import json
+from lambda_actor.actor_driver import *
+import boto3
+import logging
+from lambda_actor.actor_driver import *
+from lambda_actor.actor_executor import *
 
-    #actor_driver(bucket="rescala-configuration", prefix="lambda_actor", conf_filename="sample_actor.json", driver_trigger_message_str=driver_trigger_message_str)
- 
+bucket = "captool"
+prefix = "conf"
+actor_conf_file = "actor_conf.json"
+
+def lambda_handler(event, context):
+    print(event)
+    event_source = event["Records"][0]["eventSource"]
+    if event_source == "aws:s3":
+        key = event["Records"][0]["s3"]["object"]["key"]
+        trigger_name = key.split(".")[0].split("/")[-1]
+        print(trigger_name)
+        print("event_source from s3 end")
+        # init func
+        actor_driver_starter(bucket=bucket, prefix=prefix, actor_conf_file=actor_conf_file, trigger_name=trigger_name)
+    elif event_source == "aws:sqs":
+        driver_trigger_message_str = event["Records"][0]["body"]
+        actor_driver(bucket="captool", prefix="conf", actor_conf_file="actor_conf.json", driver_trigger_message_str=driver_trigger_message_str)
+        print("event_source from sqs end")
+    else:
+        driver_trigger_message_str = None
+        actor_driver(bucket="captool", prefix="conf", actor_conf_file="actor_conf.json", driver_trigger_message_str=driver_trigger_message_str)
+        print("event_source from manural end")
+        
     # TODO implement
     return {
         'statusCode': 200,
