436318918a42705177c0e2102faf60a4f1b057c7
RV <46471006+rv-rescala@users.noreply.github.com>
2022-01-21 19:10:22 +0900

    update

--- scripts/test_all.sh
+++ scripts/test_all.sh
@@ -1,5 +1,5 @@
 
-for text in abema_list      abema_top       cmoa_list       cmoa_top        comick_list     comick_top      ebook_list      ebook_top       unext_list      unext_top; do
+for text in abema_list      abema_top       cmoa_list       cmoa_top        comick_list     comick_top      ebook_list      ebook_top       unext_list      unext_top comick_list_desktop unext_list_desktop; do
   echo $text 
   python captool/main.py $text ./conf ./dataset
 done
